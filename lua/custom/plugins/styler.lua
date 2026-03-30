d72fc0e48194d653be0e7e31ed3857db3802c56b
laurentfg <laurent_ferland_girouard@hotmail.com>
2026-03-30 10:49:27 -0400

    fix to make it work

--- lua/custom/plugins/styler.lua
+++ lua/custom/plugins/styler.lua
@@ -1,5 +1,5 @@
 return {
-  { 'bluz71/vim-moonfly-colors', name = 'moonfly' },
+  --{ 'bluz71/vim-moonfly-colors', name = 'moonfly' },
   "folke/styler.nvim",
   config = function()
     require("styler").setup({
